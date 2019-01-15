fb8ff504ba8c273577f060129b953663345885a5
Lassiter Gregg <lassitergregg@gmail.com>
2019-01-15 14:56:04 -0600

    Added failing test for testing format based on jruby/jruby issue 5556.

---
+++ test/jruby/test_format.rb
@@ -0,0 +1,12 @@
+require 'test/unit'
+
+# Test that core class methods are correctly raising errors for incorrect call
+# arities.
+class TestFormat < Test::Unit::TestCase
+  # Test Case based on https://github.com/jruby/jruby/issues/5556
+  def test_sprintf_one
+    n = 97.65625
+    s = format("%f %f %.1f", n, n.round(1), n).to_s
+    assert(s === "97.656250 97.700000 97.7", "Test failed, value of #{s} did not match '97.656250 97.700000 97.7'.")
+  end
+end
